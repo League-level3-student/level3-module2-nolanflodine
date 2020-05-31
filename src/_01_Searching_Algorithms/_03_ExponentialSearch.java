143bcfb4eddecd4e21e212f2fd18130c95cc3e85
Floyo <Floyo@DESKTOP-G71OOBP.attlocal.net>
2020-05-31 16:54:06 -0700

    Searching Algorithms Done

--- src/_01_Searching_Algorithms/_03_ExponentialSearch.java
+++ src/_01_Searching_Algorithms/_03_ExponentialSearch.java
@@ -1,6 +1,6 @@
 package _01_Searching_Algorithms;
 
-public class _03_ExponentialSearch {
+public class _03_ExponentialSearch extends _01_BinarySearch{
 	
 	// Exponential search is super efficient for large data sets by
 	// optimizing the binary search. It, like the binary search, only
@@ -12,13 +12,24 @@ public class _03_ExponentialSearch {
 	{
 		// 2. Check if the array element at 0 is the value.
 		//    If it is, then return 0.
-
+		if(array[0] == value) {
+			return 0;
+		}
 		// 3. create an integer called counter and initialize it to 1;
-		
+		int counter = 1;
 		//4. make while loop that checks for two conditions:
 		//    	counter is less than the length of the array,
 		//		and the array element at counter is less than or equal to value
-		
+		while(counter<array.length&&array[counter]<=value) {
+			counter = counter*2;
+		}
+		int minimum;
+		if(counter<array.length) {
+			minimum = counter;
+		}
+		else {
+			minimum = array.length-1;
+		}
 			//5. inside the while loop, double the value of counter.
 
 		//6. create an integer called minimum
@@ -35,6 +46,6 @@ public class _03_ExponentialSearch {
 		//		the value to be found
 		
 		//10. return the result
-		return 0;
+		return binarySearch(array, counter/2, minimum, value);
 	}
 }
--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -11,23 +11,70 @@ class _04_SearchTest {
 	@Test
 	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
+		String[]words1 = {"hello", "hi", "bye"};
+		String value = "hello";
+		String[]words2 = {"hello", "hi", "bye"};
+		String value2 = "hi";
+		String[]words3 = {"hello", "hi", "bye"};
+		String value3 = "bill";
+		assertEquals(_00_LinearSearch.linearSearch(words1, value), 0);
+		assertEquals(_00_LinearSearch.linearSearch(words2, value2), 1);
+		assertEquals(_00_LinearSearch.linearSearch(words3, value3), -1);
+
 	}
 
 	@Test
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
-	}
+		int[]array1 = {0,1,2,4};
+		int start1 = 0;
+		int end1 = 3;
+		int value1 = 0;
+		int[]array2 = {1,5,9};
+		int start2 = 0;
+		int end2 = 3;
+		int value2 = 5;
+		int[]array3 = {10,20,30,40,50};
+		int start3 = 0;
+		int end3 = 4;
+		int value3 = 7;
+		assertEquals(_01_BinarySearch.binarySearch(array1, start1, end1, value1), 0);
+		assertEquals(_01_BinarySearch.binarySearch(array2, start2, end2, value2), 1);
+		assertEquals(_01_BinarySearch.binarySearch(array3, start3, end3, value3), -1);
+
+		}
+	
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+		int[]array1 = {0,1,2,3};
+		int value1 = 0;
+		int[]array2 = {1,5,9};
+		int value2 = 5;
+		int[]array3 = {10,20,30,40,50};
+		int value3 = 7;
+		assertEquals(_02_InterpolationSearch.interpolationSearch(array1, value1),0);
+		assertEquals(_02_InterpolationSearch.interpolationSearch(array2, value2),1);
+		assertEquals(_02_InterpolationSearch.interpolationSearch(array3, value3),-1);
+
 	}
 	
 	@Test
 	public void testExponentialSearch() {
 		//4. use the assertEquals method to test your exponential search method.
 		//   remember that the array must be sorted
+		int[]array1 = {0,1,2,3};
+		int value1 = 0;
+		int[]array2 = {1,5,9};
+		int value2 = 5;
+		int[]array3 = {10,20,30,40,50};
+		int value3 = 7;
+		assertEquals(_03_ExponentialSearch.exponentialSearch(array1, value1),0);
+		assertEquals(_03_ExponentialSearch.exponentialSearch(array2, value2),1);
+		assertEquals(_03_ExponentialSearch.exponentialSearch(array3, value3),-1);
+
 	}
 }
